c9bbe8692a0042d5f39e07be7eda1c7ae6947aa5
venkatreddy279 <45058968+venkatreddy279@users.noreply.github.com>
2024-01-31 16:17:07 -0600

    added commands on start container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull rowdy279/simple-python-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 rowdy279/simple-python-app:latest
